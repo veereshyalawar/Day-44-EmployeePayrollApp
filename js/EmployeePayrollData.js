c3fd6a2e2b9967a5ca54b6cc6eca72bd2e6aa9eb
veeresh <veereshyalawar2@gmail.com>
2022-04-13 00:01:04 +0530

    Added updated EmployeePayrollApp data

---
+++ js/EmployeePayrollData.js
@@ -0,0 +1,84 @@
+class EmployeePayrollData {
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "NAME is Incorrect";
+    }
+
+    get profileImage() {
+        return this._profileImage;
+    }
+
+    set profileImage(profileImage) {
+        this._profileImage = profileImage;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        var today = new Date();
+        const one_month_ago = new Date(today.setDate(today.getDate() - 30));
+        today = new Date();
+        if (today < startDate || startDate < one_month_ago) {
+            throw 'Start date is invalid!';
+        } else {
+            this._startDate = startDate;
+        }
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ", Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+    }
+}
